464947155aac9edaecafeaf01dca2a2e172bac32
rizaf23 <riza.mangao23@gmail.com>
2026-07-22 10:37:41 +0800

    removed homepage and update fixtures

--- fixtures/test.fixtures.ts
+++ fixtures/test.fixtures.ts
@@ -1,20 +1,14 @@
 import { test as base, expect } from '@playwright/test';
-import { HomePage } from '../pages/HomePage';
 import { SpotTheBugsPage } from '../pages/SpotTheBugsPage';
 
 type MyFixtures = {
-homePage: HomePage;
 spotTheBugsPage: SpotTheBugsPage;
 };
 
 export const test = base.extend<MyFixtures>({
-homePage: async ({ page }, use) => {
-await use(new HomePage(page));
-},
-
-spotTheBugsPage: async ({ page }, use) => {
-await use(new SpotTheBugsPage(page));
-},
+  spotTheBugsPage: async ({ page }, use) => {
+    await use(new SpotTheBugsPage(page));
+  },
 });
 
 export { expect };
